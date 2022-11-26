c89977499000dc7ac19084d9b522fe7b4fe1c4b1
SalehMasjidi <salehmasjidi@gmail.com>
2022-11-26 10:43:45 +0700

    initial commit

---
+++ computer_final/main.js
@@ -0,0 +1,7 @@
+
+         var typed = new Typed('.input', {
+            strings: ["Solar System", "Earth", "Planets", "Sun"],
+            typeSpeed: 90,
+            backSpeed:90,
+            loop:true,
+        });
